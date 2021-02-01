4c43a4b4eb2705cc8f817fd97cf3fccc13e641c7
Jlobblet <jlobblet@gmx.com>
2021-02-01 15:07:16 +0000

    New approach for calculating energy in Metropolis algorithm

--- IsingModel/Metropolis.fs
+++ IsingModel/Metropolis.fs
@@ -7,21 +7,28 @@ open Tensor
 
 let inline private (%%) a b = (a + b) % b
 
-let cartesianProduct seqs =
-    Seq.foldBack
-        (fun elem acc ->
-            seq {
-                for x in elem do
-                    for y in acc -> x :: y
-            })
-        seqs
-        (Seq.singleton [])
-
 let metropolis2d (constants: Constants.Constants) (spins: Tensor<float>) =
     let J = constants.J
     let mu = constants.mu
     let H = constants.H
     let kT = constants.kT
+
+    let adjacencyMatrix =
+        let i =
+            Tensor<float>.identity HostTensor.Dev spins.Shape.[0]
+
+        (i |> TensorExtensions.roll [| 1L; 0L |])
+        + (i |> TensorExtensions.roll [| -1L; 0L |])
+
+    let calcEnergy s =
+        let neighbourSum =
+            adjacencyMatrix .* s + s .* adjacencyMatrix
+
+        (-0.5 * J * spins * neighbourSum - mu * H * spins)
+        |> Tensor.sum
+        |> fun s -> s / float (Tensor.nElems spins)
+
+    printfn "%A" adjacencyMatrix
 
     let inner (random: Random) =
         let rec loop iterations (s: Tensor<float>) energies magnetisations =
@@ -31,21 +38,14 @@ let metropolis2d (constants: Constants.Constants) (spins: Tensor<float>) =
                 let coords =
                     s
                     |> Tensor.shape
-                    |> List.map (fun e -> random.NextDouble() * float (e - 1L) |> round |> int64)
+                    |> List.map (fun e ->
+                        random.NextDouble() * float (e - 1L)
+                        |> round
+                        |> int64)
 
                 let current = Tensor.get s coords
 
-                let neighbours =
-                    [ -1L; 0L; 1L ]
-                    |> Seq.replicate (Tensor.nDims s)
-                    |> cartesianProduct
-                    |> Seq.filter (fun l -> l |> List.filter ((<>) 0L) |> List.length = 1)
-                    |> Seq.map
-                        (fun l ->
-                            List.zip3 coords l (Tensor.shape s)
-                            |> List.map (fun (c, o, s) -> (c - o + s) % s))
-                    |> Seq.map (Tensor.get s)
-
+                let neighbours = TensorExtensions.neighbours coords s
 
                 let dE =
                     2.0
@@ -55,12 +55,13 @@ let metropolis2d (constants: Constants.Constants) (spins: Tensor<float>) =
                 let probabilityFlip =
                     if dE <= 0.0 then 1.0 else exp (-dE / kT)
 
-                if random.NextDouble() <= probabilityFlip then Tensor.set s coords -current
+                if random.NextDouble() <= probabilityFlip
+                then Tensor.set s coords -current
 
                 loop
                     (iterations - 1L)
                     s
-                    (Functions.calculateEnergy constants s :: energies)
+                    (calcEnergy s :: energies)
                     (Functions.calculateMagnetisation s
                      :: magnetisations)
 
