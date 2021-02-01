16b24be16d259feeb953c7903b1943e20e3e90a8
Jlobblet <jlobblet@gmx.com>
2021-02-01 15:06:49 +0000

    Update tryRoll

--- IsingModel/TensorExtensions.fs
+++ IsingModel/TensorExtensions.fs
@@ -8,15 +8,17 @@ let tryRoll<'a> (offset: int64 list) (tensor: Tensor<'a>): Result<Tensor<'a>, st
     match (tensor |> Tensor.nDims) = (offset |> List.length) with
     | false -> Result.throw "oh no"
     | true ->
-        Tensor.zerosLike tensor
-        |> HostTensor.mapi
-            (fun newCoords _ ->
-                let oldCoords =
-                    (newCoords |> List.ofArray, offset, tensor |> Tensor.shape)
-                    |||> List.zip3
-                    |> List.map (fun (c, o, s) -> (c - o + s) % s)
+        let shape = tensor |> Tensor.shape
+        let aShape = Array.ofList shape
 
-                Tensor.get tensor oldCoords)
+        HostTensor.init shape (fun newCoords ->
+            let oldCoords =
+                (newCoords, offset, aShape)
+                |||> Array.zip3
+                |> Array.map (fun (c, o, s) -> (c - o + s) % s)
+                |> List.ofArray
+
+            Tensor.get tensor oldCoords)
         |> Result.result
 
 let roll offset tensor =
